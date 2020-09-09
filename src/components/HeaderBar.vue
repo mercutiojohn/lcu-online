51ee195e2a3fbe98d7029075a3a2748705f18f79
mercutio <gycabc123@qq.com>
2020-09-09 17:26:33 +0800

    headerBar

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="header-bar-container">
+  <div :class="'header-bar-container'+handleShowBg()">
     <ul class="header-bar-tablist">
       <router-link to="/">
-        <li class="header-bar-tabitem">首页</li>
+        <li :class="'header-bar-tabitem'+handleChangeFontColor()">首页</li>
       </router-link>
       <router-link to="/test">
-        <li class="header-bar-tabitem">学习</li>
+        <li :class="'header-bar-tabitem'+handleChangeFontColor()">学习</li>
       </router-link>
       <router-link to="/fanya">
-        <li class="header-bar-tabitem">数字聊大</li>
+        <li :class="'header-bar-tabitem'+handleChangeFontColor()">数字聊大</li>
       </router-link>
       <router-link to="/classic">
-        <li class="header-bar-tabitem">经典</li>
+        <li :class="'header-bar-tabitem'+handleChangeFontColor()">经典</li>
       </router-link>
     </ul>
-    <SearchBar />
-    <ClockBox />
+    <SearchBar :bgEnable="bgEnable"/>
+    <ClockBox :bgEnable="bgEnable"/>
   </div>
 </template>
  
@@ -30,10 +30,43 @@ export default {
     SearchBar
   },
   data() {
-    return {};
+    return {
+      bgEnable:false
+    };
   },
   methods: {
-
+    handleScroll() {
+      let scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+      // let offsetTop = document.querySelector("body").offsetTop;
+      console.log(scrollTop);
+      // console.log(offsetTop);
+      if (scrollTop) {
+        this.bgEnable = true;
+      } else{
+        this.bgEnable = false;
+      }
+    },
+    handleShowBg(){
+      if(this.bgEnable){
+        return ' header-bar-container-bgon'
+      }else{
+        return ''
+      }
+    },
+    handleChangeFontColor(){
+      if(this.bgEnable){
+        return ' header-bar-tabitem-bgon'
+      }else{
+        return ''
+      }
+    }
+  },
+  mounted(){
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
 </script>
@@ -43,11 +76,21 @@ export default {
   user-select: none;
   padding: 20px 30px 20px 30px;
   position: sticky;
-  top: 5px;
+  top: 0px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   z-index:1000;
+  transition: all .3s ease;
+
+}
+.header-bar-container-bgon{
+  background: #ffffff;
+  /* backdrop-filter: blur(150px); */
+  transition: all .3s ease;
+  color:#000!important;
+  padding: 5px 30px 5px 30px;
+
 }
 .header-bar-tablist {
   display: flex;
@@ -62,6 +105,11 @@ export default {
   font-weight: 400;
   border-radius: 15px;
   text-shadow: 0 5px 10px #00000083;
+}
+.header-bar-tabitem-bgon {
+  color: black;
+  text-shadow: none;
+
 }
 .header-bar-tabitem:hover {
   /* color: #000; */
--- src/pages/Fanya.vue
+++ src/pages/Fanya.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="fanya">
+      <div class="fanya-top-bar">返回</div>
         <!-- <iframe class="fanya-view" src="https://mooc2-ans.chaoxing.com/visit/interaction" frameborder="0"></iframe> -->
         <!-- <iframe class="fanya-view" src="https://mooc2-ans.chaoxing.com/visit/interaction" frameborder="0"></iframe> -->
         <iframe class="fanya-view" src="http://one.lcu.edu.cn/tp_up" frameborder="0"></iframe>
@@ -19,11 +20,18 @@ export default {
 <style>
 .fanya{
     width:100vw;
-    height:100%;
+    height:100vh;
+    position: fixed;
+    top:0;
+    left: 0;
 }
 .fanya-view{
     width:100vw;
-    height:100%;
+    height:90%;
     background: #ffffff00;
+}
+.fanya-top-bar{
+  height:10%;
+  background: #000;
 }
 </style>
