fc066ce3168af1b384696b4ee3318dcd770e2bfe
mercutio <gycabc123@qq.com>
2020-11-07 18:39:13 +0800

    search player

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,13 +2,22 @@
   <div id="searchbar">
     <transition name="fade">
       <div class="searchbar-wrap" v-show="searchWrapDisplay">
-        <div class="searchbar-suggestions"></div>
+        <ul class="searchbar-engines srarchbar-wrap-items">
+              <li class="searchbar-engines-item" v-for="(item,index) in targetUrl" :key="index" @click="changeEngine(index)">
+                <div class="searchbar-engines-item-icon"></div>
+                <!-- <span :class="'searchbar-engines-item-title' + (currUrl==index)?' searchbar-engines-item-title-active':''">{{item.title}}</span> -->
+                <span :class="'searchbar-engines-item-title'">{{item.title}}</span>
+              </li>
+        </ul>
+        <div class="searchbar-suggestions srarchbar-wrap-items" v-if="suggestions"></div>
+        <!-- <div class="searchbar-recommends srarchbar-wrap-items" v-for="i in 100" :key="i"></div> -->
+        
       </div>
     </transition>
     <div :class="'searchbar-container'+handleChangeBarColor()" v-on:click="changeWrapState('display')">
       <i :class="'el-icon-search searchbar-icon'+handleChangeBarIconColor()"></i>
       <form target="_blank" autocomplete="off" :action="targetUrl[currUrl].url">
-        <input :class="'searchbar-input'+handleChangeBarFontColor()" type="text" autocomplete="off" :name="targetUrl[currUrl].queryWord" :placeholder="targetUrl[currUrl].title">
+        <input :class="'searchbar-input'+handleChangeBarFontColor()" type="text" autocomplete="off" :name="targetUrl[currUrl].queryWord" :placeholder="targetUrl[currUrl].placeholder">
       </form>
     </div>
   </div>
@@ -20,20 +29,24 @@ export default {
   name: "SearchBar",
   data(){
     return{
-      currUrl:1,
+      // currIndex:0,
+      currUrl:0,
       targetUrl: [{
-        title: "百度一下，你就知道",
-        url: "http://www.baidu.com/s",
-        queryWord: "wd",
-      },{
-        title: "秘迹搜",
-        url: "https://mijisou.com/",
+        title: "必应",
+        url: "https://cn.bing.com/search",
         queryWord: "q",
+        placeholder: "Microsoft Bing 必应搜索"
       }],
-      searchWrapDisplay: false
+      searchWrapDisplay: false,
+      suggestions: false
     }
   },
   methods:{
+    getList() {
+      var searchEngines = require("@/assets/data/searchEngines.json");
+      this.currUrl = searchEngines.currUrl;
+      this.targetUrl = searchEngines.targetUrl;
+    },
     changeWrapState(item) {
         if(item=='display'){
       if (this.searchWrapDisplay == true) {
@@ -68,7 +81,22 @@ export default {
       }else{
         return ''
       }
+    },
+    suggestCallBack(queryWord){
+      
+      window.baidu = {
+          sug: function(json) {
+              console.log(json)
+          }
+      }
+    },
+    changeEngine(index){
+      this.currUrl = index;
+      // this.currIndex = index;
     }
+  },
+  created(){
+    this.getList();
   }
 };
 </script>
@@ -156,22 +184,84 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  pointer-events:auto;
+  overflow: scroll!important;
 }
-
-.searchbar-suggestions{
-  margin-top:90px;
+.srarchbar-wrap-items{
   width:70vw;
   max-width:500px;
-  height: 100px;
+  /* height: 100px; */
   border-radius: 20px;
   background: #ffffff23;
+  margin-bottom:10px;
+  flex-shrink: 0;
+}
+.searchbar-suggestions{
+  /* margin-top:90px; */
+  height: 200px;
+}
+.searchbar-engines{
+  display: flex;
+  margin-top:90px;
+  padding: 5px 0;
+  overflow: scroll;
 }
 
+.searchbar-engines-item{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 6px;
+  border-radius: 15px;
+}
+.searchbar-engines-item:nth-child(1){
+  margin-left: 5px;
+}
+.searchbar-engines-item:hover{
+  transform: scale(1.05);
+  background: #ffffff45;
+  cursor: pointer;
+  /* margin-right: 10px; */
+}
+.searchbar-engines-item,.searchbar-engines-item:hover{
+  
+  transition: all .2s ease;
+}
+.searchbar-engines-item:active{
+  transform: scale(.98);
+  transition: all .2s ease;
+}
+
+.searchbar-engines-item-icon{
+  width: 50px;
+  height: 50px;
+  background: #ffffff56;
+  border-radius: 10px;
+}
+.searchbar-engines-item-title{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  color:#ffffff78;
+  width: 55px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space:nowrap;
+  height: 17px;
+  font-size: 14px;
+}
+.searchbar-engines-item-title-active{
+  color:white;
+}
+.searchbar-recommends{
+  height:500px;
+}
 @media screen and (max-width: 600px){
   #searchbar{
     width:100%;
   }
-  .searchbar-container,.searchbar-suggestions{
+  .searchbar-container,.srarchbar-wrap-items{
     width:95%;
   }
 }
